5ce696f2098a1a1ece9d80b02b04bcfe892ba14b
Charanvardhan <charanvardhanreddym@gmail.com>
2025-08-25 12:16:37 -0400

    Here we go with our TO-DO List

---
+++ travel-list-05/src/PackingList.js
@@ -0,0 +1,68 @@
+import { useState } from "react";
+
+export default function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearButton,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+  // const [clear, setClear] = useState(false);
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  // function setSortByHandler(e) {
+  //   setSortBy(e.target.value);
+  // }
+
+  return (
+    <div className="list">
+      <ul>
+        {" "}
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
+        ))}{" "}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input"> Sort by Input order </option>
+          <option value="description"> Sort by description </option>
+          <option value="packed"> Sort by packed status </option>
+        </select>
+        <button onClick={onClearButton}>Clear List</button>
+      </div>
+    </div>
+  );
+}
+
+function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>
+        {item.packed ? "✅" : "❌"}
+      </button>
+    </li>
+  );
+}
